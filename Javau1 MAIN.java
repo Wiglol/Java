1f914d843a8d355c98dcfafbcb97557cb8787bd0
Wiglol <Wiggo.santti@gmail.com>
2024-09-12 12:38:56 +0300

    Add file
    YEye

---
+++ Javau1 MAIN.java
@@ -0,0 +1,67 @@
+
+public class JavaBooleans {
+
+    public static void main(String args[]) {
+              String firstname = "Wiglo";
+System.out.println(firstname);
+
+String lastname = "Santti"; 
+
+System.out.println (lastname);
+System.out.println (firstname + lastname);
+
+int myNum = 16; 
+
+
+System.out.println(myNum); 
+
+double myNum2 = 189;
+
+System.out.println(myNum2);
+
+boolean student = true; 
+boolean NotStudent = false;
+
+System.out.println(student);
+System.out.println(NotStudent);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+        
+
+        
+
+
+
+
+   
+    
+    
+
+
+
+
+    
+

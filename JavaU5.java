b2142a5f83dc4410281e7085a38e8b708775d794
Wiglol <Wiggo.santti@gmail.com>
2024-09-13 10:54:26 +0300

    Add files via upload

---
+++ JavaU5.java
@@ -0,0 +1,57 @@
+
+package Desktop;
+
+
+public class JavaU5 {
+
+  
+    public static void main(String args[]) {
+        
+        int vitsord = 37; 
+        
+           if (vitsord >= 90 ) {
+               System.out.println("vitsord 5");
+           }else  if (vitsord >= 80 ) {
+               System.out.println("vitsord 4");
+           }else  if (vitsord >= 70 ) {
+               System.out.println("vitsord 3");
+           }else  if (vitsord >= 60 ) {
+               System.out.println("vitsord 2");
+           }else  if (vitsord >= 25 ) {
+               System.out.println("vitsord 1");
+           }else  if (vitsord >= 0 ) {
+               System.out.println("vitsord 0");
+           
+           } 
+                   
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+       
+    }
+}

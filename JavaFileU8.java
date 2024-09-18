35bdf73774a29147d4a8dd86e22acdb454dd7711
Wiglol <Wiggo.santti@gmail.com>
2024-09-18 13:31:07 +0300

    Add files via upload

---
+++ JavaFileU8.java
@@ -0,0 +1,24 @@
+
+public class JavaFileU8 {
+
+  
+    public static void main(String args[]) {
+        
+        
+  
+  
+  
+     for (int i = 0; i < 30; i++) {
+  if (i == 12) {
+    break;
+  }
+  System.out.println(i);
+}
+        
+        System.out.println("Loop terminated early due to break statement");
+    }
+    
+    
+    
+    
+}
